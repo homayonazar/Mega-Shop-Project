3013b61025cdb7629d2969592b4dc7751a1ac5ab
homayonazar <amirrezaabdollahi01@gmail.com>
2025-09-09 15:46:27 +0300

    Header & Navbar Completed

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,13 +1,54 @@
+import { useEffect, useState } from "react";
 import Logo from "../assets/images/Logo.png"
 import Container from "./Container"
+import Navbar from "./Navbar"
 function Header() {
+
+    const [darkMode, setDarkMode] = useState(() => {
+        const saved = localStorage.getItem("theme");
+        return saved ? saved === "dark" : false;
+    });
+
+    useEffect(() => {
+        const root = document.documentElement;
+        if (darkMode) {
+            root.classList.add("dark");
+            localStorage.setItem("theme", "dark");
+        } else {
+            root.classList.remove("dark");
+            localStorage.setItem("theme", "light");
+        }
+    }, [darkMode]);
+
+    const toggleDarkMode = () => setDarkMode(v => !v);
+
+
+
+
+
+
     return (
-        <div className="shadow">
+        <div className="shadow bg-[var(--bg)]">
             <div className="Upper_section_of_header w-full h-7 flex justify-between border-b-1 border-gray-200">                <div className="Left_side ms-3">
                 <i className="fas fa-light fa-phone"></i>
                 <span className="text-gray-700 m-1"> Call us : +90 (531) 123 4567</span>
             </div>
                 <div className="flex items-center gap-6 text-gray-600">
+
+                    <button
+                        className="Day|Night_button cursor-pointer"
+                        type="button" onClick={toggleDarkMode}
+                        title={darkMode ? "Switch to light" : "Switch to dark"}>
+
+                        {darkMode ? (
+                            <p> <i className="far fa-regular fa-sun me-1"></i>Day</p>
+                        ) : (
+                            <p> <i className="far fa-light fa-moon me-1"></i>Night</p>
+                        )}
+                    </button>
+
+
+
                     <button className="flex items-center gap-2 cursor-pointer">
                         <i className="fas fa-light fa-language "></i>
                         <span className="">Language:</span>
@@ -18,6 +59,7 @@ function Header() {
                         <i className="far fa-user text-lg"></i>
                         <span className="font-medium me-3">Sign-in</span>
                     </button>
+
                 </div>
             </div>
             <Container>
@@ -25,21 +67,33 @@ function Header() {
                     <div className="Logo_of_Site w-50 py-2">
                         <img src={Logo} alt="logo picture" />
                     </div>
-                    <div className="Search_input ">
-                        <input type="text" placeholder="what are you looking for" />
+                    <div className="Search_box flex w-full max-w-lg mx-auto border rounded-full overflow-hidden">
+                        <button className="px-4 py-2 bg-white flex items-center gap-1">
+                            All categories ▼
+                        </button>
+
+                        <input
+                            type="text"
+                            placeholder="Search for products"
+                            className="flex-1 px-5 py-3 outline-none ]"
+                        />
+                        <button className="px-4 py-2 bg-gray-600 text-white">
+                            <i className="fas fa-light fa-magnifying-glass"></i>
+                        </button>
                     </div>
                     <div className="Calling">
                         <div className="up mb-2">
                             <i className="fas fa-light fa-phone"></i>
-                        <span className="text-gray-500 m-1"> Call us : +90 (531) 123 4567</span>
+                            <span className="text-gray-500 m-1"> Call us : +90 (531) 123 4567</span>
                         </div>
                         <div className="down">
                             <i className="fas fa-light fa-envelope"></i>
-                        <span className="text-gray-500 m-1"> Our E-mail : contact@homayonazar.com</span>
+                            <span className="text-gray-500 m-1"> Our E-mail : contact@homayonazar.com</span>
                         </div>
                     </div>
                 </div>
             </Container>
+            <Navbar />
         </div>
     )
 }
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,7 +2,7 @@ import Header from "../components/Header"
 
 function Home() {
   return (
-    <div>
+    <div className="bg-[var(--bg)] w-full h-1000">
         <Header/>
         
     </div>
