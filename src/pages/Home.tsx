354ac728445578429456324977a8398154966cad
homayonazar <amirrezaabdollahi01@gmail.com>
2025-09-10 17:58:54 +0300

    working on homePage sliders

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,3 +1,4 @@
+import Container from "../components/Container"
 import Footer from "../components/Footer"
 import Header from "../components/Header"
 
@@ -5,6 +6,23 @@ function Home() {
   return (
     <div className="bg-[var(--bg)] w-full">
         <Header/>
+
+        <Container>
+            <div className="Slide_menu_section flex flex-row gap-6 mt-10">
+            <div className="sideMenu w-1/4 bg-amber-300 h-130"></div>
+            <div className="middlegalery w-1/2 bg-amber-500 h-130"></div>
+            <div className="rightGalery w-1/4 bg-amber-800 h-130"></div>
+
+        </div>
+        </Container>
+
+
+
+
+
+
+
+
         <br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br />
         <br /><br /><br /><br /><br /><br />
         <Footer/>
